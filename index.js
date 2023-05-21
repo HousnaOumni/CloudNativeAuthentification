98ffb0077e86cf6895697efefabc271a95ee26eb
HousnaOumni <117484393+HousnaOumni@users.noreply.github.com>
2023-05-21 11:28:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 const express = require("express");
-//const bodyParser = require("body-parser");
 const RouterAuth = require("./Routes/routes.js");
-//const ejs = require("ejs");
+
 const app = express();
 const db = require("./db.js");
-//app.use(bodyParser.json());
+
 app.use(express.json());
 app.use("/api", RouterAuth);
-
-/* app.set("view engine", "ejs");
-
-let nom = "ALAMI Amina";
-let villes = ["Agadir", "Ouarzazate", "Tanger"];
-let html = ejs.render('<%= villes.join(", "); %>', { villes: villes });
-let resultat = ejs.render("<%= name; %>", { name: nom });
-app.get("/", (req, res) => {
-  res.send(html );
-}); */
 
 app.listen(4000, () => {
   console.log("le serveur est demare sur le port 4000");
 });
 
-module.exports = app;
+
